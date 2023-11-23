e1fe9da6cdc2bcf0ad718449d52f98c4af46478e
PaulXu-cn <>
2023-11-24 00:40:16 +0800

    完成 go client

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/PaulXu-cn/smallchat
+
+go 1.18
+
+require (
+	github.com/pkg/term v1.1.0
+	golang.org/x/term v0.14.0
+)
+
+require golang.org/x/sys v0.14.0 // indirect
--- smallchat-client.go
+++ smallchat-client.go
@@ -5,61 +5,9 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
-	"strings"
-	"syscall"
-	"unsafe"
+
+	"golang.org/x/term"
 )
-
-// Terminal settings
-type Termios struct {
-	Iflag  uint32
-	Oflag  uint32
-	Cflag  uint32
-	Lflag  uint32
-	Line   uint8
-	Cc     [32]uint8
-	Ispeed uint32
-	Ospeed uint32
-}
-
-const (
-	TCGETS = 0x5401
-	TCSETS = 0x5402
-)
-
-func getTermios(fd int) (*Termios, error) {
-	termios := &Termios{}
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), TCGETS, uintptr(unsafe.Pointer(termios)))
-	if err != 0 {
-		return nil, err
-	}
-	return termios, nil
-}
-
-func setTermios(fd int, termios *Termios) error {
-	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), TCSETS, uintptr(unsafe.Pointer(termios)))
-	if err != 0 {
-		return err
-	}
-	return nil
-}
-
-func setRawMode(fd int) error {
-	termios, err := getTermios(fd)
-	if err != nil {
-		return err
-	}
-
-	termios.Iflag &^= syscall.BRKINT | syscall.ICRNL | syscall.INPCK | syscall.ISTRIP | syscall.IXON
-	termios.Oflag &^= syscall.OPOST
-	termios.Cflag |= syscall.CS8
-	termios.Lflag &^= syscall.ECHO | syscall.ICANON | syscall.IEXTEN | syscall.ISIG
-	termios.Cc[syscall.VMIN] = 1
-	termios.Cc[syscall.VTIME] = 0
-
-	return setTermios(fd, termios)
-}
 
 func main() {
 	if len(os.Args) != 3 {
@@ -67,38 +15,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	port, err := strconv.Atoi(os.Args[2])
+	conn, err := net.Dial("tcp", os.Args[1]+":"+os.Args[2])
 	if err != nil {
-		fmt.Println("Invalid port number")
-		os.Exit(1)
-	}
-
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", os.Args[1], port))
-	if err != nil {
-		fmt.Println("Error connecting to server:", err)
+		fmt.Println("Error connecting:", err)
 		os.Exit(1)
 	}
 	defer conn.Close()
 
-	if err := setRawMode(int(os.Stdin.Fd())); err != nil {
-		fmt.Println("Error setting raw mode:", err)
-		os.Exit(1)
-	}
+	var tty = term.NewTerminal(conn, "> ")
 
 	go func() {
-		scanner := bufio.NewScanner(os.Stdin)
+		scanner := bufio.NewScanner(conn)
 		for scanner.Scan() {
-			text := scanner.Text()
-			if _, err := conn.Write([]byte(text + "\n")); err != nil {
-				fmt.Println("Error writing to server:", err)
-				os.Exit(1)
-			}
+			fmt.Println(scanner.Text())
 		}
 	}()
 
-	scanner := bufio.NewScanner(conn)
+	// get stdin in
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		text := scanner.Text()
-		fmt.Println(strings.TrimRight(text, "\n"))
+		tty.Write([]byte(scanner.Text() + "\n"))
 	}
+
+	return
 }
